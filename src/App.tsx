a6f0b2c12a5e9b3aa11a62b57797c2921158f3d8
Francisco Vinicios Alves de Sousa Sena <francisco.viniciosti@gmail.com>
2022-06-22 22:39:50 -0300

    third day

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
+import { ApolloProvider, gql, useQuery } from "@apollo/client"
 import { client } from "./lib/apollo"
 import { Event } from "./pages/Event"
 
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from "./Router"
 
 
 
 export function App() {
-
-
- 
   return (
-    <div>
-      <Event/>
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -16,13 +17,13 @@ export function Lesson({ title, type, availableAt, slug }: LessonProps) {
   })
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">
         
         {availableDateFormatted}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -41,6 +42,6 @@ export function Lesson({ title, type, availableAt, slug }: LessonProps) {
 
         <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
-    </a>
+    </Link>
   )
 }
